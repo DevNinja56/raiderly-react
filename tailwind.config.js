771f2c491ca1cfda2d05279ad6c5f06431b05506
Abdul Aleem <aleemjunaid254@gmail.com>
2023-11-23 17:51:55 +0500

    remove-some-styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ export default {
         lightOrangeColor:"#FF7425E5",
       },
       padding:{
-        ten:"10px",
         minusTenPixel:"-10px",
       },
       maxWidth:{
@@ -20,12 +19,7 @@ export default {
         contentSmallScreenWidth:"92%",
       },
       height:{
-        vh40:"40vh",
-        vh50:"50vh",
         vh60:"60vh",
-        vh70:"70vh",
-        vh80:"80vh",
-        vh90:"90vh",
         vh100:"100vh",
         vh140:"140vh",
       },
